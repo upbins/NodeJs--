77dc8dba270be1bc8f733197536c47468868d4db
upbins <550440098@qq.com>
2018-01-18 19:10:54 +0800

    ejs使用

--- 7.NodeJS中的get,post参数/server01.js
+++ 7.NodeJS中的get,post参数/server01.js
@@ -25,19 +25,46 @@
 var http = require("http")
 var url = require("url")
 var ejs = require("ejs")
+//ejs常用标签
+//<% %>流程控制标签,例如里面加个for
+//<%= %>输出标签(原文输出HTML标签)
+//<%- %>输出标签(Html会被浏览器解释
 http.createServer(function (req,res) {
     //router.statics(req,res,"static")
+    res.writeHead(200,{"Content-type":"text/html;charest='utf-8'"})
     var pathname = url.parse(req.url).pathname
-    //根据不同的url来操作不同的路由操作
-    if (pathname === "/login"){
-        console.log("login")
+    //console.log(pathname)
+    if (pathname != "/favicon.ico") {
+        //根据不同的url来操作不同的路由操作
+        if (pathname === "/login"){
+            console.log("login")
+            var data_temp = "我是一个数据"
+            var list = [
+                "1111",
+                "2222",
+                "3333",
+            ]
+            ejs.renderFile("views/login.ejs",{
+                msg:data_temp,
+                list:list,
+            },function (err,data) {
+                res.end(data);
+            })
+        }
+        else if(pathname === "/register"){
+            console.log("register")
+            var register_data  = "我是个注册数据"
+            ejs.renderFile("views/register.ejs",{
+                msg:register_data,
+            },function (err,data) {
+                res.end(data);
+            })
+        }
+        else{
+            console.log("other")
+        }
     }
-    else if(pathname === "/register"){
-        console.log("register")
-    }
-    else{
-        console.log("other")
-    }
+
 }).listen(8000)
 
 console.log("sever running 8000")
